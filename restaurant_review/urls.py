d119afcccf7a28957f3f76b2f2f7a06bedea4459
alozanorius <129527387+alozanorius@users.noreply.github.com>
2024-06-14 13:36:40 -0400

    Update urls.py

--- restaurant_review/urls.py
+++ restaurant_review/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "restaurant_review"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>/', views.details, name='details'),
-    path('create', views.create_restaurant, name='create_restaurant'),
-    path('add', views.add_restaurant, name='add_restaurant'),
-    path('review/<int:id>', views.add_review, name='add_review'),
+    path('', views.mirar_directorio, name='mirar_directorio'),
+    path('descargar_confirmacion/', views.descargar_confirmacion, name='descargar_confirmacion'),
+    path('descargar_excel/', views.descargar_excel, name='descargar_excel'),
 ]
