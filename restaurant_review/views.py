020557c049fa7c315150195bb09f16f9c950df73
alozanorius <129527387+alozanorius@users.noreply.github.com>
2024-06-14 13:35:27 -0400

    Update views.py

--- restaurant_review/views.py
+++ restaurant_review/views.py
@@ -1,14 +1,52 @@
+import base64
+from django.views import generic 
 from django.db.models import Avg, Count
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, JsonResponse, HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.cache import cache_page
+from django.conf import settings
+from . import utils
 
 from restaurant_review.models import Restaurant, Review
 
 # Create your views here.
+def descargar_confirmacion(request):
+    return render(request, 'restaurant_review/descargar_confirmacion.html')
+
+def descargar_excel(request):
+    base64_encoded = request.session.get('excel_data')
+    if base64_encoded:
+        excel_data = base64.b64decode(base64_encoded)
+        response = HttpResponse(excel_data, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="datos_de_muestra.xls"'
+        return response
+    else:
+        return HttpResponse("No se encontró el archivo para descargar.")
+
+def mirar_directorio(request):
+    if request.method == 'POST':
+        files = request.FILES.getlist('files')
+        lista_valores = []
+        for file in files:
+            print(file)
+            print("\n\n")
+            try:
+                valores = utils.extraccion_total(file)
+                lista_valores.append(valores)
+            except:
+                print(f"Incompatibilidad de archivo: {file}")
+        response_stream = utils.crear_excel_xls_con_archivo(lista_valores, "salida_django_xls")
+        base64_encoded = base64.b64encode(response_stream.getvalue()).decode('utf-8')
+        request.session['excel_data'] = base64_encoded
+        print("Llegué acá")
+        redirect_url = reverse('restaurant_review:descargar_confirmacion')
+        return JsonResponse({'success': True, 'redirect_url': redirect_url})
+    print(settings.DATA_UPLOAD_MAX_NUMBER_FILES)
+    return render(request, 'restaurant_review/directory_read_bis.html')
+
 
 def index(request):
     print('Request for index page received')
