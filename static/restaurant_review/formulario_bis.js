ccfc16337d639a1ae3211dc4198747008ea262d2
alozanorius <129527387+alozanorius@users.noreply.github.com>
2024-06-15 12:56:53 -0400

    Update formulario_bis.js

--- static/restaurant_review/formulario_bis.js
+++ static/restaurant_review/formulario_bis.js
@@ -10,8 +10,6 @@ function setupFormHandlers() {
     var loadingMessage = document.getElementById('loading-message');
     
 
-    //    Esto "traspasa el click" desde nuestro botón (custom-button) al
-    //  original (file-input que tenemos escondido)
     customButton.addEventListener('click', function() {
         inputElement.click();
     });     
@@ -28,16 +26,42 @@ function setupFormHandlers() {
     function handleInputChange() {
         var fileList = document.getElementById('file-list');
         fileList.innerHTML = ''; // Limpiar la lista previa
-        let countPDFs = 0; // Contador para archivos PDF
+        let countPDFs = 0; // Contador para archivos PDF válidos
+        const maxFileSize = 614400; // Tamaño máximo en bytes (600 KB)
+        const maxValidFiles = 25; // Máximo número de archivos válidos
+        let exceededLimit = false; // Indicador si se superó el límite
+        let excedetamanho = false;
+    
         Array.from(inputElement.files).forEach(function(file) {
+            // Solo procesar hasta 30 archivos PDF válidos
             if (file.name.toLowerCase().endsWith('.pdf')) {
-                var li = document.createElement('li');
-                li.textContent = file.name + ' (tamaño: ' + file.size + ' bytes)';
-                fileList.appendChild(li);
-                countPDFs++; // Incrementar contador si el archivo es PDF
+                if (file.size <= maxFileSize) { // Verificar el tamaño del archivo
+                    if (countPDFs < maxValidFiles) {
+                        var li = document.createElement('li');
+                        li.textContent = file.name + ' (tamaño: ' + file.size + ' bytes)';
+                        fileList.appendChild(li);
+                        countPDFs++; // Incrementar contador si el archivo es PDF y tiene tamaño adecuado
+                    } else {
+                        // Marcar que se ha superado el límite
+                        exceededLimit = true;
+                    }
+                } else {
+                    excedetamanho = true;
+                }
             }
         });
-        uploadButton.style.display = countPDFs > 0 ? 'block' : 'none'; 
+        // Mostrar mensajes si algún archivo supero el tamaño de 600K y/o se supera el límite de 30 archivos válidos
+        if (excedetamanho) {
+            var li = document.createElement('li');
+            li.textContent = ' Se descartaron PDFs de tamaño superior a 600K';
+            fileList.appendChild(li);
+        }
+        if (exceededLimit) {
+            var li = document.createElement('li');
+            li.textContent = ' El máximo número de PDFs válidos es 25, por favor cargue el resto desde otra carpeta';
+            fileList.appendChild(li);
+        }       
+        uploadButton.style.display = countPDFs > 0 ? 'block' : 'none';
     }
 
     // Remover y volver a agregar el manejador de eventos de clic para uploadButton
@@ -53,12 +77,21 @@ function setupFormHandlers() {
         var formData = new FormData(form);
         var csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
         formData.append('csrfmiddlewaretoken', csrfToken);
-        
+        let countPDFs = 0; // Contador para archivos PDF válidos
+        const maxFileSize = 614400; // Tamaño máximo en bytes (600 KB)
+        const maxValidFiles = 25; // Máximo número de archivos válidos        
         Array.from(inputElement.files).forEach(function(file) {
             if (file.name.endsWith('.pdf')) {
-                formData.append('files', file, file.name);
+                if (countPDFs < maxValidFiles) {
+                    if (file.size <= maxFileSize) { // Verificar el tamaño del archivo
+                        formData.append('files', file, file.name);
+                        countPDFs++; 
+                        }
+                    }
+                }
             }
-        });
+        );
+
 
         // Realizar la solicitud de carga con fetch
         fetch(form.action, {
